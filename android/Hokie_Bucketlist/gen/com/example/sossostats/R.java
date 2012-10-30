ac1ab1368490f2b882e90d1fc3ea55c2aee9a59b
Anthony Sosso <asosso@friendeo.com>
2012-10-30 01:36:35 -0400

    Login screen complete, needs to be wired in properly

--- android/Hokie_Bucketlist/gen/com/example/sossostats/R.java
+++ android/Hokie_Bucketlist/gen/com/example/sossostats/R.java
@@ -15,27 +15,28 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int TextView03=0x7f070007;
+        public static final int TextView03=0x7f070009;
         public static final int fetchbutton=0x7f070001;
         public static final int hrp=0x7f070003;
-        public static final int item_id=0x7f07000a;
+        public static final int item_id=0x7f07000b;
         public static final int listView1=0x7f070006;
         public static final int menu_settings=0x7f07000d;
         public static final int pgf=0x7f070005;
         public static final int radiogroup=0x7f070002;
         public static final int rdt=0x7f070004;
-        public static final int server=0x7f070008;
-        public static final int shareIt=0x7f07000b;
+        public static final int server=0x7f07000a;
+        public static final int shareIt=0x7f070008;
         public static final int statListView=0x7f07000c;
         public static final int tab=0x7f070000;
-        public static final int username=0x7f070009;
+        public static final int username=0x7f070007;
     }
     public static final class layout {
         public static final int activity_display_club_stats=0x7f030000;
         public static final int activity_main=0x7f030001;
         public static final int listview=0x7f030002;
-        public static final int main=0x7f030003;
-        public static final int statlistview=0x7f030004;
+        public static final int login=0x7f030003;
+        public static final int main=0x7f030004;
+        public static final int statlistview=0x7f030005;
     }
     public static final class menu {
         public static final int activity_display_club_stats=0x7f060000;
